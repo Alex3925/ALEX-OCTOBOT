2355dd5ec9f34657c95ada11c918c5dbbd455680
Alex3925 <120993369+Alex3925@users.noreply.github.com>
2024-05-22 05:41:26 +0800

    Update help.js

--- cmds/help.js
+++ cmds/help.js
@@ -1,10 +1,10 @@
 module.exports = {
     description: "Show Commands and the descriptions",
     role: "user",
-    credits: "rejardgwapo",
+    credits: "Alex Jhon Ponce",
     cooldown: 16,		
     execute(api, event, args, commands) {
-        let helpMessage = '𝙷𝚒! 𝚃𝚑𝚒𝚜 𝙱𝚘𝚝 𝚒𝚜 𝚌𝚛𝚎𝚊𝚝𝚎𝚍 𝚊𝚝 𝚢𝚎𝚝𝚊𝚗𝚘𝚝𝚑𝚎𝚛𝚏𝚋𝚋𝚘𝚝.𝚣𝚢𝚊.𝚖𝚎. 𝙷𝚎𝚛𝚎 𝚊𝚛𝚎 𝚊𝚕𝚕 𝚝𝚑𝚎 𝚌𝚘𝚖𝚖𝚊𝚗𝚍𝚜 𝚒𝚗𝚜𝚝𝚊𝚕𝚕𝚎𝚍 𝚘𝚗 𝚝𝚑𝚒𝚜 𝚜𝚎𝚛𝚟𝚎𝚛\n';
+        let helpMessage = '𝙷𝚒! 𝚃𝚑𝚒𝚜 𝙱𝚘𝚝 𝚒𝚜 𝚌𝚛𝚎𝚊𝚝𝚎𝚍 by Alex!. 𝙷𝚎𝚛𝚎 𝚊𝚛𝚎 𝚊𝚕𝚕 𝚝𝚑𝚎 𝚌𝚘𝚖𝚖𝚊𝚗𝚍𝚜 𝚒𝚗𝚜𝚝𝚊𝚕𝚕𝚎𝚍 𝚘𝚗 𝚝𝚑𝚒𝚜 𝚜𝚎𝚛𝚟𝚎𝚛\n';
         helpMessage += '💮═══════════════💮\n';
         commands.forEach((command, name) => {
             helpMessage += `𝙽𝚊𝚖𝚎: ${name}\n`;
@@ -13,7 +13,7 @@ module.exports = {
             helpMessage += `Credits: ${command.credits}\n`;
         helpMessage += '💮═══════════════💮\n';
         });
-        helpMessage += '💬https://facebook.com/yetanotherfbbot';
+        helpMessage += '💬https://facebook.com/100085861488156';
         api.sendMessage(helpMessage, event.threadID);
     }
 };
